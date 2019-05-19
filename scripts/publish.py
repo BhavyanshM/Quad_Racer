609f65aacc53f13839125e0005fbafef3c5c143b
BhavyanshM <bhavyanshmishra@gmail.com>
2019-05-19 02:00:30 -0500

    Add the IRMarkerArray msgs

--- scripts/publish.py
+++ scripts/publish.py
@@ -2,8 +2,9 @@
 import rospy
 from std_msgs.msg import String
 from mav_msgs.msg import RateThrust
+from flightgoggles.msg import IRMarker, IRMarkerArray
 
-def talker():
+def pubRateThrust():
     pub = rospy.Publisher('output/rateThrust', RateThrust, queue_size=10)
     rospy.init_node('talker', anonymous=True)
     rate = rospy.Rate(10) # 10hz
@@ -12,22 +13,24 @@ def talker():
         thr_msg.header.frame_id = "uav/imu"
         thr_msg.header.stamp = rospy.Time.now()
 
-        pitch = 0.3;
-        roll = 0.3;
-        yaw = 0.3;
-        vertical = 10;
+        marker = IRMarker()
+
+        pitch = 0.0;
+        roll = -0.1;
+        yaw = 0.5;
+        vertical = 11;
 
         thr_msg.angular_rates.x = roll;
         thr_msg.angular_rates.y = pitch;
         thr_msg.angular_rates.z = yaw;
         thr_msg.thrust.z = vertical;
 
-        rospy.loginfo(thr_msg)
+        rospy.loginfo(marker)
         pub.publish(thr_msg)
         rate.sleep()
 
 if __name__ == '__main__':
     try:
-        talker()
+        pubRateThrust()
     except rospy.ROSInterruptException:
         pass
